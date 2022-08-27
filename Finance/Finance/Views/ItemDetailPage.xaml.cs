cde820a642ddd10ed1882b452340a53a36e33ab3
bm1905 <bijay.maharjan5@gmail.com>
2022-08-27 17:40:05 -0500

    first commit

---
+++ Finance/Finance/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Finance.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Finance.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
