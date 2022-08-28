899e78560079cb0aa9e3cbbde1765d23e776af4e
bm1905 <bijay.maharjan5@gmail.com>
2022-08-28 11:03:30 -0500

    Set up project structure

---
+++ src/Client/FinancePlanner.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FinancePlanner.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Client/FinancePlanner.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FinancePlanner.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
