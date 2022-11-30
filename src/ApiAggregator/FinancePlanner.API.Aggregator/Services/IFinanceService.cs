c476c1246b9cdf1800d6839c07adc82f491d4469
bm1905 <bijay.maharjan5@gmail.com>
2022-11-29 20:36:56 -0600

    Add delete service

--- src/ApiAggregator/FinancePlanner.API.Aggregator/Services/IFinanceService.cs
+++ src/ApiAggregator/FinancePlanner.API.Aggregator/Services/IFinanceService.cs
@@ -9,5 +9,5 @@ public interface IFinanceService
     Task<IncomeInformationResponse> SavePay(PayInformationRequest request, string? userId, int? payId, int? incomeId);
     Task<List<PayInformationResponse>> GetPayList(string userId, int? payId);
     Task<List<IncomeInformationResponse>> GetIncomeList(string userId, int? incomeId);
-    Task<bool> DeletePay(string userId, int payId);
+    Task<bool> DeletePay(string userId, int payId, int incomeId);
 }
